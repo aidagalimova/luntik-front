0e3574f701c1bdac6ddc71e5c482914e89ed0d19
aidagalimova <aidagalimova03@gmail.com>
2021-06-25 16:14:00 +0300

    add tickets date

--- src/components/tickets-list/index.jsx
+++ src/components/tickets-list/index.jsx
@@ -5,17 +5,33 @@ import { Row, Col } from "antd";
 import "./index.scss";
 
 function TicketsList() {
-    const ticketsList = tickets.map((ticket) => {
-        return (<Ticket ticket={ticket} />)
+    var options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    };
+    const ticketsList = tickets.map((dayTickets) => {
+        console.log(dayTickets)
+        return (
+            <Row key={dayTickets.date} className="tickets-row">
+                <Col span={24}>
+                    <h1 className="text">{dayTickets.date.toLocaleString("ru", options)}</h1>
+                </Col>
+                {dayTickets.tickets.map((ticket) => {
+                    console.log(ticket);
+                    return (<Ticket ticket={ticket} key={ticket.id} />)
+                })}
+            </Row>
+        )
     });
 
     return (
         <>
             <Row className="tickets-list">
                 {ticketsList.length === 0 ?
-                    <h1 className="text">У вас нет билетов</h1> :
+                    <h1 className="text title">У вас нет билетов</h1> :
                     <>
-                        <Col span={24}> <h1 className="text">Ваши билеты</h1></Col>
+                        <Col span={24}> <h1 className="text title">Ваши билеты</h1></Col>
                         {ticketsList}
                     </>
                 }
@@ -27,51 +43,58 @@ function TicketsList() {
 
 const tickets = [
     {
-        id: 1,
-        name: "Лука",
-        start: "14:00",
-        date: new Date(),
-        hall: 1,
-        row: 8,
-        place: 12
-    },
-    {
-        id: 2,
-        name: "Лука",
-        start: "14:00",
-        date: new Date(),
-        hall: 1,
-        row: 8,
-        place: 12
-    },
-    {
-        id: 3,
-        name: "Лука",
-        start: "14:00",
-        date: new Date(),
-        hall: 1,
-        row: 8,
-        place: 12
-    },
-    {
-        id: 4,
-        name: "Лука",
-        start: "14:00",
-        date: new Date(),
-        hall: 1,
-        row: 8,
-        place: 12
-    },
-    {
-        id: 4,
-        name: "Лука",
-        start: "14:00",
-        date: new Date(),
-        hall: 1,
-        row: 8,
-        place: 12
-    }
+        date: new Date(2021, 6, 25),
+        tickets:
+            [
+                {
+                    id: 1,
+                    name: "Лука",
+                    start: "14:00",
+                    hall: 1,
+                    row: 8,
+                    place: 12
+                },
+                {
+                    id: 2,
+                    name: "Лука",
+                    start: "14:00",
+                    hall: 1,
+                    row: 8,
+                    place: 12
+                },
+                {
+                    id: 3,
+                    name: "Лука",
+                    start: "14:00",
+                    hall: 1,
+                    row: 8,
+                    place: 12
+                },
+                {
+                    id: 4,
+                    name: "Лука",
+                    start: "14:00",
+                    hall: 1,
+                    row: 8,
+                    place: 12
+                }
 
-]
+            ]
+    },
+    {
+        date: new Date(2021, 6, 26),
+        tickets:
+            [
+                {
+                    id: 5,
+                    name: "Лука",
+                    start: "14:00",
+                    hall: 1,
+                    row: 8,
+                    place: 12
+                }
+
+            ]
+    }];
 
 export default TicketsList;

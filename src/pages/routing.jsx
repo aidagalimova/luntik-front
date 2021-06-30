0b4fca238f7b58e7f10474e483b9da0204f268ac
aidagalimova <aidagalimova03@gmail.com>
2021-06-30 13:50:47 +0300

    add admin page and admin routing

--- src/pages/routing.jsx
+++ src/pages/routing.jsx
@@ -3,12 +3,28 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import MainPage from "./main";
 import FilmPage from "./film";
 import ProfilePage from "./profile";
-const Routing = () => (
-    <Router>
-        <Route path="/" component={MainPage} exact/>
-        <Route path="/profile" component={ProfilePage} exact />
-        <Route  path="/films/:id"  component={FilmPage}/>
-    </Router>
-);
+import AdminPage from "./admin";
+import AddFilmPage from "./add-film";
+const isAdmin = true;
+const Routing = ({ isAuth }) => {
+    if (!isAdmin) {
+        return (
+            <Router>
+                <Route path="/" component={MainPage} exact />
+                <Route path="/profile" component={ProfilePage} exact />
+                <Route path="/films/:id" component={FilmPage} />
+            </Router>
+        )
+    } else {
+        return (
+            <Router>
+                <Route path="/" component={MainPage} exact />
+                <Route path="/profile" component={AdminPage} exact />
+                <Route path="/films/:id" component={FilmPage} />
+                <Route path="/addfilm" component={AddFilmPage} />
+            </Router>
+        )
+    }
+};
 
 export default Routing;

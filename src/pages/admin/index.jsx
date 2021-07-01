c6fdaaf9786a643baa0f60411e71edc0b79d7472
aidagalimova <aidagalimova03@gmail.com>
2021-07-01 22:45:16 +0300

    add admin exit

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -3,6 +3,8 @@ import { Button } from "antd";
 import "./index.scss";
 import { ReactComponent as DeleteIcon } from "./svg/delete.svg";
 import { Link} from "react-router-dom";
+import { useDispatch } from "react-redux";
+
 const films = [
     { id: 1, name: "Luka" },
     { id: 2, name: "Luka" },
@@ -11,16 +13,22 @@ const films = [
 ];
 
 function AdminPage() {
+    const dispatch = useDispatch()
+    // var obj = JSON.parse(sessionStorage.getItem('user'));
+    const exit = () => {
+        dispatch(deleteUser());
+        window.location.reload();
+    }
+
     const filmsList = films.map((film) => {
         return (<div key={film.id}><h1 className="film-name-text">{film.name}</h1> <DeleteIcon className="delete-icon" /></div>)
     })
     return (
         <div className="films-div page">
-
             <Button type="primary" className="add-film-btn">
              <Link to="/addfilm"><h2 className="btnText">Добавить фильм</h2></Link>
             </Button>
-            <h1 className="exit-text">Выйти</h1>
+            <h1 className="exit-text" onClick={exit}>Выйти</h1>
             <div className="films-list-div">
                 {filmsList}
             </div>
